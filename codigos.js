118c86fe7d517e74d9a57f509b0ff897868d6673
JesusAybar <orichalcos17@gmail.com>
2021-10-16 12:35:50 -0400

    the codes

---
+++ codigos.js
@@ -0,0 +1,24 @@
+function digitsAreEven (n)
+{if(n >= 99 || n <= 10){throw new Error("n should have 2 digits");}
+const isEven = n => n % 2 === 0;
+const firstdigit = Math.floor (n/10);
+const seconddigit = n % 10;
+return isEven(firstdigit) && isEven(seconddigit);}
+
+
+function LastDigitIs4 (x)
+{const LastDigit = x % 10;
+return (LastDigit === 4);}
+
+
+function 3DigitsNumber (x)
+{return (x >= 100 || x <= 999 )}
+
+
+function IsNegative (x)
+{return (x < 0)}
+
+
+function Sum2DigitsNumber (x)
+{if(n >= 99 || n <= 10){throw new Error("n should have 2 digits")}
+const SumDigits = Math.floor (x % 10) + (x / 10)}
