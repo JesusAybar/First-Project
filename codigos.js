36558dcd904a9cb1fcb2d7eecd0de541a2ad4821
JesusAybar <orichalcos17@gmail.com>
2021-10-16 12:57:11 -0400

    fifth function fixed

--- codigos.js
+++ codigos.js
@@ -20,5 +20,5 @@ function IsNegative (x)
 
 
 function SumTwodigitsNumber (x)
-{if(x >= 99 || x <= 10){throw new Error("n should have 2 digits")}
+{if(x >= 99 || x <= 10){throw new Error("x should have 2 digits")}
 const SumDigits = Math.floor (x % 10) + (x / 10)}
